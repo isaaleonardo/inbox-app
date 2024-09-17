a9783305aee11f867b4fa1c91dc7a9f33be5f771
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-09-17 12:44:14 -0400

    Create styles from Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,14 @@
+import './Search.css'
+
 export function Search () {
   return (
-    <form>
-      <input type='text' placeholder='Enter search term' />
-      <input type='submit' value='Search' />
+    <form className='form'>
+      <label className='label'>
+        <div className='search-icon'>
+          <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='icon icon-tabler icons-tabler-outline icon-tabler-search'><path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0' /><path d='M21 21l-6 -6' /></svg>
+        </div>
+        <input type='text' placeholder='Enter search term' />
+      </label>
     </form>
   )
 }
