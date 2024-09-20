3e942cc7121bd205e12a94aa93463bf3a9dd9f5e
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-09-19 21:43:15 -0400

    Avoid editing when task completed

--- src/components/TodoElement.jsx
+++ src/components/TodoElement.jsx
@@ -18,6 +18,12 @@ export function TodoElement ({ todo, onToggle, onEdit }) {
     }
   }
 
+  const handleClick = () => {
+    if (!todo.completed) {
+      setIsEditing(true)
+    }
+  }
+
   return (
     <>
       <button className='check' onClick={onToggle}>
@@ -44,7 +50,7 @@ export function TodoElement ({ todo, onToggle, onEdit }) {
             </form>
             )
           : (
-            <div onClick={() => setIsEditing(true)}>
+            <div onClick={handleClick}>
               <p>{todo.name}</p>
               <div className='edit'>
                 <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='icon icon-tabler icons-tabler-outline icon-tabler-edit'><path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1' /><path d='M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z' /><path d='M16 5l3 3' /></svg>
