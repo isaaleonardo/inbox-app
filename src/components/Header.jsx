de35db0c08d5a8d90b00cefd49861991218f5b9e
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-09-23 13:20:50 -0400

    Create confirmation modal for clean list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,33 @@
 import './Header.css'
 
 export function Header ({ setTodos }) {
+  const dialog = document.querySelector('dialog')
+
+  const handleOpen = () => {
+    dialog.showModal()
+  }
+
+  const handleClose = () => {
+    dialog.close()
+  }
+
+  const handleConfirm = () => {
+    setTodos([])
+    handleClose()
+  }
+
   return (
     <header>
+      <dialog>
+        <p>Are you sure you want to delete all tasks?</p>
+        <div className='buttons'>
+          <button onClick={handleConfirm}>Confirm</button>
+          <button onClick={handleClose}>Cancel</button>
+        </div>
+      </dialog>
+
       <h1>Inbox</h1>
-      <button onClick={() => setTodos([])}>
+      <button onClick={handleOpen}>
         Clean List
       </button>
     </header>
